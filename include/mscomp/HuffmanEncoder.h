3b3e29ed690eec604825d06f2fd9761768a31d57
Jeffrey Bush <jeff@coderforlife.com>
2015-03-13 18:32:35 -0700

    Adding comment about bzip2 inspiration and removing sorting library.

--- include/mscomp/HuffmanEncoder.h
+++ include/mscomp/HuffmanEncoder.h
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+// Adapted from bzip2. See http://www.bzip.org/.
+
 #ifndef MSCOMP_HUFFMAN_ENCODER
 #define MSCOMP_HUFFMAN_ENCODER
 
